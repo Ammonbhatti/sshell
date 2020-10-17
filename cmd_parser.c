9476deca98f95fcb55603fab8500c928426556f0
bsher <bsher@ad3.ucdavis.edu@pc32.cs.ucdavis.edu>
2020-10-16 20:16:26 -0700

    file redirection

--- cmd_parser.c
+++ cmd_parser.c
@@ -1,11 +1,14 @@
 #include "cmd_parser.h"
 #include <stdio.h>
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
 
 //Pass struct by reference
 void cmd_parser(cmd_t* vessel, char* raw)
 {
 	
 	char *argument, *ui;		//used to process args including argv[0]
+	char *nameOfOutputFile;
 
 	/* Get rid of '\n' at the end of the command */
 	ui  = strchr(raw, '\n');        //ui points to '\n'
@@ -44,22 +47,28 @@ void cmd_parser(cmd_t* vessel, char* raw)
 	{
 		//Handle file redirection
 		int count =0;
-			
-		argument = strtok(raw, " ");
+		int fd;
+		
+		argument = strtok(raw, ">");
+		nameOfOutputFile = strtok(NULL, ">");
+		argument = strtok(argument, " ");
 		strcpy(vessel->exec, "/bin/");
 		strcat(vessel->exec, argument);
-			vessel->args[count++] = argument;
+		    vessel->args[count++] = argument;
 		        	
-		
-			
 		strcpy(vessel->args[0], argument);  
-		argument = strtok(NULL, ">");
+		argument = strtok(NULL, " ");
 		    while(argument != NULL)
 		{
 			vessel->args[count++] = argument;
-			argument = strtok(NULL, ">"); 	
-		}
-		printf("%s",vessel->args[1]);
-		// printf("%s","heeeee");
+			argument = strtok(NULL, " "); 	
+		}	
+		        	
+		// printf("%s\n", nameOfOutputFile);
+
+		fd = open(nameOfOutputFile, O_WRONLY | O_CREAT, 0644);	
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+
 	}
 }
